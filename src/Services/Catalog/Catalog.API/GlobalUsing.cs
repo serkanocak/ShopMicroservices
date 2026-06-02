a5916b2470f95331a0cfea8fc753c65ed725b81c
serkanocak <serkanocak@gmail.com>
2026-06-02 16:19:46 +0200

    catalog api and docker container published

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,16 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+global using Marten.Pagination;
+global using Marten.Schema;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using Catalog.API.Data;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
